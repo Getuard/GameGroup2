34f8f4c89cf1bfcbd01aae0a74c98069cde8d8c0
Getuard <getuardlloqani21@outlook.com>
2023-11-06 21:02:21 +0000

    BaseFeatures
    Implemented coins, coin success, UI display, enemies, controls(left,right,forward,backwards,jump)

--- GitHubExample/Assets/Scenes/Scripts/PlayerController.cs
+++ GitHubExample/Assets/Scenes/Scripts/PlayerController.cs
@@ -1,90 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float moveSpeed;
-    //public Rigidbody theRB;
     public float jumpForce;
     public CharacterController controller;
     private Vector3 moveDirection;
     public float gravityScale;
-    public Transform pivot;
-    public float rotateSpeed;
-
-    public GameObject playerModel;
-
     
     public float knockBackForce;
-    public float KnockBackTime;
-    private float KnockBackCounte;
+    public float knockBackTime;
+    private float knockBackCounter;
+
+    private bool isJumping = false;
+    private float jumpBufferTime = 0.1f; // Time in seconds to buffer the jump
+    private float jumpBufferCounter;
 
     // Start is called before the first frame update
     void Start() {
-        //theRB = GetComponent<Rigidbody>(); 
         controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
-
-        // if(Input.GetButtonDown("Jump")){
-        //     theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
-
-        //}
-        //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
-        
-        if(KnockBackCounte <= 0)
+        if (knockBackCounter <= 0)
         {
+            if (controller.isGrounded)
+            {
+                moveDirection.y = 0f;
+            }
 
-        float yStore = moveDirection.y;
-        moveDirection = (Input.GetAxis("Vertical") * moveSpeed * transform.forward) + (Input.GetAxis("Horizontal") * moveSpeed * transform.right);
-        moveDirection = moveDirection.normalized * moveSpeed;
-        moveDirection.y = yStore;
+            moveDirection.x = Input.GetAxis("Horizontal") * moveSpeed;
+            moveDirection.z = Input.GetAxis("Vertical") * moveSpeed;
+            moveDirection = transform.TransformDirection(moveDirection);
 
-        if(controller.isGrounded)
+            if (controller.isGrounded && Input.GetButtonDown("Jump"))
+            {
+                isJumping = true;
+                jumpBufferCounter = jumpBufferTime;
+            }
+            
+            if (jumpBufferCounter > 0)
+            {
+                jumpBufferCounter -= Time.deltaTime;
+            }
+        }
+        else
         {
-            moveDirection.y = 0.0f; 
-            if (Input.GetButtonDown("Jump"))
-             {
-                moveDirection.y = jumpForce;
-             }
+            knockBackCounter -= Time.deltaTime;
         }
-        }else{
-            KnockBackCounte -= Time.deltaTime;
-        }
-
-        // if(Input.GetButtonDown("Jump"))
-        // {
-        //     moveDirection.y = jumpForce;
-        // }
-        moveDirection.y += Physics.gravity.y * gravityScale;
-        controller.Move(moveDirection * Time.deltaTime);
-
-        // //Move the player in different directions based on camera look directions
-        // if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
-        // {
-        //     transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
-        //     Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
-        //     playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
-        // }
-    
     }
+
+    void FixedUpdate()
+    {
+        if (knockBackCounter <= 0)
+        {
+            if (isJumping && jumpBufferCounter > 0)
+            {
+                moveDirection.y = jumpForce;
+                isJumping = false;
+            }
+
+            moveDirection.y += (Physics.gravity.y * gravityScale * Time.fixedDeltaTime);
+            controller.Move(moveDirection * Time.fixedDeltaTime);
+        }
+    }
+
     public void Knockback(Vector3 direction)
     {
-        KnockBackCounte = KnockBackTime;
-
-        // direction = new Vector3(1f, 1f, 1f);
-
+        knockBackCounter = knockBackTime;
         moveDirection = direction * knockBackForce;
         moveDirection.y = knockBackForce;
-
-
     }
 }
